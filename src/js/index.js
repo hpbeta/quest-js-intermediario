175de435bd38717991b83b3d5b9d4f1c477f4262
Joao Lucas <hpbeta16.l@gmail.com>
2023-12-26 11:46:52 -0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+const inputText = document.querySelectorAll('.input-text')
+const btn = document.querySelector('.btn')
+const campoPreenchido = document.querySelector('.campo-preenchido')
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault()
+    inputText.forEach(input => {
+        if (input.value === "") {
+            input.classList.add('campo-requered')
+            input.nextElementSibling.classList.remove('ativo')
+        } else {
+            input.classList.remove('campo-requered')
+            input.nextElementSibling.classList.add('ativo')
+            input.classList.add('campo-preenchido')
+        }
+    }
+    )
+})
+
+
+
+
